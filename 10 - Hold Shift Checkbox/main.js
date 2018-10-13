0de99c3553ed85688deb433e651165eeb6bad568
Desy Armariena <desy.armariena@gmail.com>
2018-10-13 22:39:29 +0700

    Hold Shift Checkbox - Flagged between checked checkbox to be checked

--- 10 - Hold Shift Checkbox/main.js
+++ 10 - Hold Shift Checkbox/main.js
@@ -1,7 +1,28 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+let lastChecked;
+
 function handleCheck(e) {
-    console.log(e);
+    
+    let inBetween = false;
+    //cek jika shift key ditekan sekaligus check (bukan uncheck)
+    if(e.shiftKey && this.checked && lastChecked !== undefined){
+        checkboxes.forEach(checkbox => {
+            // console.log(checkbox);
+            if(checkbox === this || checkbox === lastChecked){
+                //menandakan jika checkbox yang di loop berada di antara checkbox yang di klik
+                inBetween = !inBetween;
+                // console.log("start to check");
+            }
+            if(inBetween){
+                checkbox.checked = true;
+            }
+        });
+
+    }
+
+
+    lastChecked = this;
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
